ede470a6a99e9c94176f1f196d01a28c3d62a3d9
BarkerProoks <jonparkerbrooks@gmail.com>
2025-02-05 14:21:53 -0600

    add login.js

---
+++ static/js/login.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const error = document.querySelector('.error');
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', function(event) {
+        const formData = new FormData(form);
+
+        fetch('/api/login', {
+            headers: { 'Content-Type': 'application/json' },
+            method: "POST",
+            body: JSON.stringify({
+                username: formData.get('username'),
+                password: formData.get('password')
+            })
+        }).then(async response => {
+            if (response.status == 400) {
+                error.innerText = "invalid request";
+            } else if (response.status == 500) {
+                error.innerText = "passwords do not match";
+            } else {
+                window.location = '/'; // send new user to home page
+            }
+        });
+
+        event.preventDefault();
+    });
+
+});
